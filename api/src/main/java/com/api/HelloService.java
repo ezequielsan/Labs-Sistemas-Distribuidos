f7ade23793c1da3885cdd2e9bbbb176390db3112
ezequielsan <ezequielsan.melo@gmail.com>
2025-07-11 19:08:33 -0300

    Implement HelloAPI with EJB and RESTful services, including HelloResource and HelloService classes

---
+++ api/src/main/java/com/api/HelloService.java
@@ -0,0 +1,10 @@
+package com.api;
+
+import javax.ejb.Stateless;
+// EJB Stateless que retorna uma mensagem
+@Stateless
+public class HelloService {
+    public String sayHello(String name) {
+    return "Olá, " + name + "! Bem-vindo à API EJB.";
+ }
+}
